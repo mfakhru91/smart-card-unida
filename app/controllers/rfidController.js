085e374af1e62b05be18bb4a34b722682f449ec9
mfakhru91 <mfakhrualfarizqi91@gmail.com>
2020-09-26 15:52:05 +0700

    init project

--- app/controllers/rfidController.js
+++ app/controllers/rfidController.js
@@ -5,6 +5,8 @@ var axios = require('axios')
 var Promise = this.Promise || require('promise');
 var agent = require('superagent-promise')(require('superagent'),Promise);
 var bodyParser = require('body-parser');
+const tok = require('../../config/token.json')
+const svcConfig = require('../../config/svc.json')
 
 var http = require('../../app')
 var io = require('socket.io').listen(http.http);
@@ -15,9 +17,9 @@ app.use(bodyParser.json());
 exports.putRfid = ( req, res, next ) => {
 	let data = {nim: req.query.nim, rfid: req.query.rfid}
   if (req.query.nim && req.query.rfid) {
-      var update = agent.put('http://api.unida.gontor.ac.id:1926/m/rfid/update')
+      var update = agent.put(svcConfig.rfid+'/m/rfid/update')
       .set('Content-Type','application/x-www-form-urlencoded')
-      .set('x-access-token','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnRJZCI6InJpemt5bXBwbCIsImNsaWVudFNlY3JldCI6ImViY0psWUx1MGciLCJpYXQiOjE1ODMxMjA4OTN9.tsGIyqeCMTkpeG2-U0td0EiUBlTbvn1w_DSerj9kVuk')
+      .set('x-access-token',tok.token)
       .buffer()
       .send(data)
       .then((res)=>{
--- app/db_config.js
+++ app/db_config.js
@@ -1,15 +1,23 @@
-var mysql = require('mysql');
+const dbConfig = require('../config/db.json');
+var mysql = require('mysql')
+var db = mysql.createPool(dbConfig)
 
-var db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "siakadunida"
-});
+db.getConnection((err, connection) => {
+    if (err) {
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            console.error('Database connection was closed.')
+        }
+        if (err.code === 'ER_CON_COUNT_ERROR') {
+            console.error('Database has too many connections.')
+        }
+        if (err.code === 'ECONNREFUSED') {
+            console.error('Database connection was refused.')
+        }
+    }
 
-db.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-});
+    if (connection) connection.release()
 
-module.exports = db;
+    return
+})
+
+module.exports = db
