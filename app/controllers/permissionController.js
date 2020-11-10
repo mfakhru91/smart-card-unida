44869367b43ad9cf6f17448ccb3a37eec4a787b1
mfakhru91 <mfakhrualfarizqi91@gmail.com>
2020-11-10 10:57:00 +0700

    edit function

--- app/controllers/permissionController.js
+++ app/controllers/permissionController.js
@@ -46,7 +46,7 @@ function set_izin(rfid,status,cb) {
 			}
 			var hasil = {
 				"status": 200,
-				"message":"anda berhasil keluar",
+				"message":"anda berhasil "+status,
 			}
 			cb (null,hasil)
 		})
@@ -61,13 +61,14 @@ function set_izin(rfid,status,cb) {
 
 exports.keluar = (req, res, next) => {
 	var rfid =  req.query.rfid
-	set_izin(rfid,function(err,res){
+	set_izin(rfid,2,function(err,hasil){
 		if(err){
 			console.log(err);
 			res.json(err)
 			res.end()
 		}
-		res.json(res)
+		res.json(hasil)
+		console.log(hasil)
 		res.end()
 	})
 
@@ -75,65 +76,14 @@ exports.keluar = (req, res, next) => {
 
 exports.masuk = (req, res, next) => {
 	var rfid =  req.query.rfid
-	var datetime = moment().format('YYYY-MM-DD HH:mm:ss')
-	console.log(rfid+' '+datetime);
-	var sql = "SELECT * FROM simak_mastermahasiswa WHERE rfid = ?"
-	var rfidData = new Promise(function(resolve, reject ) {
-		db.query(sql,rfid,function(err, results){
-			if (err){
-				console.log(err);
-				reject(err)
-			}
-			data = results[0];
-			if(data == null){
-				reject(err)
-				res.json({
-					"status": 404,
-					"message":"data tidak dimeukan",
-				})
-			}else{
-				var nim =  data.nim_mhs
-				resolve(nim);
-			}
-		})
-	})
-	rfidData.then((nim,reject) => {
-		var sql = "INSERT INTO erp_izin_harian (nim, waktu, status_izin) VALUES (?,?,'2')"
-		db.query(sql,[datetime,nim],function (err, results) {
-			if (err){
-				console.log(err)
-				reject(err)
-			}
-			data = results[0]
-			res.json({
-				"status": 200,
-				"message":"anda berhasil masuk",
-			})
-		})
-	})
-	rfidData.catch(err =>{
-		console.log(err)
-		res.json(err)
-		res.end()
-	})
-}
-exports.getData = ( req, res,  next )=> {
-	var getDataKeluar = new Promise((resolve,reject)=>{
-		var sql=" SELECT a.created_at,a.waktu,b.nim_mhs as 'nim',b.nama_mahasiswa,b.semester,c.nama_prodi FROM  erp_izin_harian a JOIN simak_mastermahasiswa b ON a.nim = b.nim_mhs JOIN simak_masterprogramstudi c ON b.kode_prodi = c.kode_prodi WHERE a.status_izin = 2 ORDER BY a.created_at DESC LIMIT 1"
-		db.query(sql,function(err,result) {
-			if (err){
-				console.log(err)
-				reject(err)
-			}
-			resolve(result)
-		})
-	})
-	getDataKeluar.then(data =>{
-		res.json(data)
-	})
-	getDataKeluar.catch(err =>{
-		console.log(err)
-		res.json(err)
+	set_izin(rfid,1,function(err,hasil){
+		if(err){
+			console.log(err);
+			res.json(err)
+			res.end()
+		}
+		res.json(hasil)
+		console.log(hasil)
 		res.end()
 	})
 }
